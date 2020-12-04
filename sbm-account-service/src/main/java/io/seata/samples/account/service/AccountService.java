a4dcf5b21c44a258a4b8bbc5f839f5da72dfe32a
934734735@qq.com <934734735@qq.com>
2020-12-04 19:54:37 +0800

    spring-boot seata demo

---
+++ sbm-account-service/src/main/java/io/seata/samples/account/service/AccountService.java
@@ -0,0 +1,32 @@
+package io.seata.samples.account.service;
+
+import io.seata.samples.account.persistence.Account;
+import io.seata.samples.account.persistence.AccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class AccountService {
+
+    private static final String ERROR_USER_ID = "1002";
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    public void debit(String userId, BigDecimal num) {
+        Account account = accountMapper.selectByUserId(userId);
+        account.setMoney(account.getMoney().subtract(num));
+
+        //TODO:扣减库存失败
+        int a = 2/0;
+
+        accountMapper.updateById(account);
+
+        if (ERROR_USER_ID.equals(userId)) {
+            throw new RuntimeException("account branch exception");
+        }
+    }
+
+}
